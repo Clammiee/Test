accf0065acdf68efdf45cf906f44b2a21631265e
Jessica St-Pierre <jessica.stpierre@hotmail.com>
2021-03-25 11:11:54 -0400

    - Implemented MoveRandomlyWithinZone

---
+++ Assets/Scripts/Step_3/MoveRandomlyWithinZone.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveRandomlyWithinZone : MonoBehaviour
+{
+    [SerializeField] private float x;
+    [SerializeField] private float y;
+    [SerializeField] private float z;
+    [SerializeField] private Vector3 randomPos;
+    [SerializeField] private float speed = 1f;
+
+    private void FixedUpdate() 
+    {
+        Move();
+    }
+
+    private void GenerateRandomPos()
+    {
+        randomPos = new Vector3(Random.Range(0f, x), Random.Range(0f, y), Random.Range(0f, z));
+    }
+
+    private void Move()
+    {
+        this.transform.position = Vector3.MoveTowards(this.transform.position, randomPos, speed * Time.deltaTime);    
+        CheckArrived();
+    }
+
+    private void CheckArrived()
+    {
+        if (Vector3.Distance(transform.position, randomPos) < 0.5f)
+        {
+            this.transform.position = randomPos;
+            GenerateRandomPos();
+        }
+    }
+}
