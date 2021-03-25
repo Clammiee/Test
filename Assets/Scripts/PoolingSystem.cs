219db6bc08d2444cafd95d58c56c8eef7e3025cc
Jessica St-Pierre <jessica.stpierre@hotmail.com>
2021-03-25 09:57:00 -0400

    - Implemented Pooling system that expands

---
+++ Assets/Scripts/PoolingSystem.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoolingSystem : MonoBehaviour
+{
+    [System.Serializable]
+    public class Pool
+    {
+        public string tag;
+        public GameObject prefab;
+        public int size;
+    }
+
+    public static PoolingSystem Instance;
+
+    public List<Pool> pools;
+    public Dictionary<string, Queue<GameObject>> pool_Dictionary;
+    public Queue<GameObject> object_Pool;
+
+    void Awake()
+    {
+        Instance = this;
+
+        pool_Dictionary = new Dictionary<string, Queue<GameObject>>();
+        foreach (Pool pool in pools)
+        {
+            object_Pool = new Queue<GameObject>();
+
+            for(int i = 0; i < pool.size; i++)
+            {
+                GameObject obj = Instantiate(pool.prefab, transform);
+                obj.SetActive(false);
+                object_Pool.Enqueue(obj);
+            }
+            pool_Dictionary.Add(pool.tag, object_Pool);
+        }
+    }
+
+    public GameObject SpawnFromPool(string tag, Vector3 position)
+    {
+        if(!pool_Dictionary.ContainsKey(tag))
+        {
+            Debug.LogWarning("Pool with tag " + tag + " doesn't exist.");
+            return null;
+        }
+
+        GameObject objectTo_Spawn = pool_Dictionary[tag].Dequeue();
+        objectTo_Spawn.SetActive(true);
+        objectTo_Spawn.transform.position = position;
+
+        pool_Dictionary[tag].Enqueue(objectTo_Spawn);
+
+        return objectTo_Spawn;
+    }
+
+    public void ExpandPool(string tag, int amount)
+    {
+        if(!pool_Dictionary.ContainsKey(tag))
+        {
+            Debug.LogWarning("Pool with tag " + tag + " doesn't exist.");
+        }
+        else 
+        {
+            for (int i = 0; i < amount; i++)
+            {
+                foreach (Pool pool in pools)
+                {
+                    if(pool.tag == tag)
+                    {
+                        GameObject obj = Instantiate(pool.prefab, transform);
+                        obj.SetActive(false);
+                        object_Pool.Enqueue(obj);
+                        break;
+                    }
+                }
+            }
+            
+        }
+    }
+}
---
+++ Assets/Scripts/TestPooling.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestPooling : MonoBehaviour
+{
+    void Start()
+    {
+        PoolingSystem.Instance.SpawnFromPool("Sphere", this.transform.position);
+        PoolingSystem.Instance.ExpandPool("Sphere", 3);
+    }
+
+    void Update()
+    {
+        
+    }
+}
